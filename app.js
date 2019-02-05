c8c81b8f5a46202ddab57c53caa34b436b3ad0bd
Grigor <grigor.crossroad@gmail.com>
2019-02-05 17:59:33 +0400

    fvfddddfflkfkffff

--- app.js
+++ app.js
@@ -159,7 +159,7 @@ function getToken() {
             // subsequent calls to getToken will return from cache.
             messaging.getToken()
                 .then(function(currentToken) {
-
+                    console.log(`Getting token ${token}`)
                     if (currentToken) {
                         sendTokenToServer(currentToken);
                         updateUIForPushEnabled(currentToken);
